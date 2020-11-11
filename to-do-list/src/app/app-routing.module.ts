d89cd5d80513bfccc6ae9ec8355485f1ad7445ca
Thalita Rodrigues Santos <trodsant@everis.com>
2020-11-11 15:04:20 -0300

    Criação de rotas

--- to-do-list/src/app/app-routing.module.ts
+++ to-do-list/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'form', component: FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const ComponentRoute = [HomeComponent, FormComponent]
--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-
 import { HeaderComponent } from './header/header.component';
-import { FormComponent } from './form/form.component';
-
+import { ComponentRoute } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     FooterComponent,
-    HomeComponent
-
     HeaderComponent,
-    FormComponent
-
+    ComponentRoute
   ],
   imports: [
     BrowserModule,
