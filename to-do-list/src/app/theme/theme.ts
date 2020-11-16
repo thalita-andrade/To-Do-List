fcd5246bc12dfb830d61cdae52772b2587e74aaa
Jessica Melise Mendonca <jmelisem@everis.com>
2020-11-16 15:58:20 -0300

    ajustando cores de contraste

--- to-do-list/src/app/theme/theme.ts
+++ to-do-list/src/app/theme/theme.ts
@@ -11,16 +11,22 @@ export const light: Theme = {
         "--default-tertiary":  "#beb0e7",
         "--default-quarternary": "white",
         "--default-quinary": "#f1f3f4",
+        "--default-senary": "#5f49a0",
+        "--default-shadow-table": "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)",
+        "--default-shadow-scrollbar": "inset 0 0 3px grey",
     }
 }
 
 export const dark: Theme = {
     name: "dark",
     properties: {
-        "--default-primary": "grey",
+        "--default-primary": "#41474d",
         "--default-secundary":  "darkgrey",
         "--default-tertiary": "lightgrey",
         "--default-quarternary": "black",
-        "--default-quinary": "#41474d",
+        "--default-quinary": "grey",
+        "--default-senary": "#35393d",
+        "--default-shadow-table": "0px 3px 1px -2px rgba(255, 255, 255, 0.2), 0px 2px 2px 0px rgba(255, 255, 255, 0.14), 0px 1px 5px 0px rgba(255, 255, 255, 0.12)",
+        "--default-shadow-scrollbar": "inset 0 0 5px whitesmoke",
     }
 }
