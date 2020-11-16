b2978749ade80526d621ef7637e6964ce4023ac4
Thalita Rodrigues Santos <trodsant@everis.com>
2020-11-16 14:24:55 -0300

    Corrigindo imports

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -11,14 +11,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
 import { MatFormFieldModule} from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
-
 
 @NgModule({
   declarations: [
@@ -36,10 +32,7 @@ import { MatCardModule } from '@angular/material/card';
     MatTableModule,
     MatButtonModule,
     MatIconModule,
-
     MatFormFieldModule,
-    MatInputModule
-
     MatInputModule,
     ReactiveFormsModule,
     MatSelectModule,
